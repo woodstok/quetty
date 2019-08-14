855b3b4c2b4ad44a737d0aff94abfe3d2d774acb
Appu Joseph <apjo@kaloom.com>
2019-08-14 13:32:45 -0400

    Add support for returning/testing errors

--- tokenize.go
+++ tokenize.go
@@ -1,25 +1,50 @@
 package main
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+)
 
 type Tokenizer interface {
-	Tokenize(input string) []string
+	Tokenize(input string) ([]string, error)
 }
 
-func Tokenize(input string, tok Tokenizer) []string {
+func Tokenize(input string, tok Tokenizer) ([]string, error) {
 	return tok.Tokenize(input)
 }
 
+// Basic Tokenizer
+// splits based on space
 type BasicTokenizer struct {
 }
 
-func (t *BasicTokenizer) Tokenize(input string) []string {
-	return strings.Split(input, " ")
+func (t *BasicTokenizer) Tokenize(input string) ([]string, error) {
+	return strings.Split(input, " "), nil
 }
 
+// Nil Tokenizer
+// Returns the input as is
 type NilTokenizer struct {
 }
 
-func (t *NilTokenizer) Tokenize(input string) []string {
-	return []string{input}
+func (t *NilTokenizer) Tokenize(input string) ([]string, error) {
+	return []string{input}, nil
+}
+
+// Regex Tokenizer
+// splits based on space
+type RegexTokenizer struct {
+	pattern string
+	re_     *regexp.Regexp
+}
+
+func (t *RegexTokenizer) Tokenize(input string) ([]string, error) {
+	var err error
+	if t.re_ == nil {
+		t.re_, err = regexp.Compile(t.pattern)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return t.re_.FindAllString(input, -1), nil
 }
--- tokenize_test.go
+++ tokenize_test.go
@@ -13,16 +13,70 @@ func assertSliceEqual(t *testing.T, want, got []string) {
 
 }
 
+func assertNoError(t *testing.T, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatalf("got an error '%v' but didnt want one", got)
+	}
+}
+
+func assertSomeError(t *testing.T, got error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("did not get an error but wanted one", got)
+	}
+}
+
+func assertError(t *testing.T, got error, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("didn't get an error but wanted one")
+	}
+
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
 func TestTokenize(t *testing.T) {
 	basicString := "This is a sentence"
+	basicStringExpected := []string{"This", "is", "a", "sentence"}
 	t.Run("Nil Tokenizer", func(t *testing.T) {
-		got := Tokenize(basicString, &NilTokenizer{})
+		got, err := Tokenize(basicString, &NilTokenizer{})
+		assertNoError(t, err)
 		assertSliceEqual(t, []string{basicString}, got)
 	})
 
 	t.Run("tokenize basic set of strings", func(t *testing.T) {
-		want := []string{"This", "is", "a", "sentence"}
-		got := Tokenize(basicString, &BasicTokenizer{})
-		assertSliceEqual(t, want, got)
+		got, err := Tokenize(basicString, &BasicTokenizer{})
+		assertNoError(t, err)
+		assertSliceEqual(t, basicStringExpected, got)
 	})
+
+	regexTests := []struct {
+		name    string
+		pattern string
+		input   string
+		err     bool
+		want    []string
+	}{
+		{name: "Word", pattern: `\w+`, input: basicString, err: false, want: basicStringExpected},
+		{name: "Number", pattern: `\d+`, input: "2 numbers in 486 sentences", err: false, want: []string{"2", "486"}},
+		{name: "Hash", pattern: `[a-f0-9A-F]{4,}\b`, input: "4c 2abd4c 4beefc1 abcdefghijk abcdef9gh", err: false, want: []string{"2abd4c", "4beefc1"}},
+		{name: "BadRegex", pattern: `[a-f0-9A-F`, input: "Does not matter", err: true, want: nil},
+	}
+
+	for _, tt := range regexTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Tokenize(tt.input, &RegexTokenizer{pattern: tt.pattern})
+			if tt.err {
+				assertSomeError(t, err)
+			} else {
+				assertNoError(t, err)
+			}
+			assertSliceEqual(t, tt.want, got)
+		})
+
+	}
+
 }
