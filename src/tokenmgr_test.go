acd690821f69e4235593b773ee188a0812869f94
woodstok <amjosep2@ncsu.edu>
2019-09-10 11:44:52 -0400

    Add number and hash tests

--- src/tokenmgr_test.go
+++ src/tokenmgr_test.go
@@ -7,20 +7,40 @@ import (
 
 func TestTokenMgr(t *testing.T) {
 	basicString := "This is a sentence 43 567 abcd dcba dead123beef non123hash"
+	wantNum := "43 567 123"
+	wantHash := "abcd dcba dead123beef"
 	t.Run("token manager will return error on no tokenizers", func(t *testing.T) {
 		_, err := NewTokenMgr(&Options{})
 		assertSomeError(t, err)
 	})
-	t.Run("basic word tokenset", func(t *testing.T) {
-		tokMgr, err := NewTokenMgr(&Options{matchWord: true})
-		assertNoError(t, err)
 
-		reader := strings.NewReader(basicString)
-		tokens, err := tokMgr.Process(reader)
-		assertNoError(t, err)
+	tokenMgrTests := []struct {
+		name      string
+		options   *Options
+		inputStr  string
+		expectErr bool
+		wantStr   string
+	}{
+		{name: "tokenize basic words", options: &Options{matchWord: true},
+			inputStr: basicString, expectErr: false, wantStr: basicString},
+		{name: "tokenize numbers", options: &Options{matchNum: true},
+			inputStr: basicString, expectErr: false, wantStr: wantNum},
+		{name: "tokenize hashes", options: &Options{matchHash: true},
+			inputStr: basicString, expectErr: false, wantStr: wantHash},
+	}
 
-		want := NewTokens(strings.Split(basicString, " "))
-		assertTokensEqual(t, want, tokens)
+	for _, tt := range tokenMgrTests {
+		t.Run(tt.name, func(t *testing.T) {
+			tokMgr, err := NewTokenMgr(tt.options)
+			assertNoError(t, err)
 
-	})
+			reader := strings.NewReader(tt.inputStr)
+			tokens, err := tokMgr.Process(reader)
+			assertNoError(t, err)
+
+			want := NewTokens(strings.Split(tt.wantStr, " "))
+			assertTokensEqual(t, want, tokens)
+		})
+
+	}
 }
